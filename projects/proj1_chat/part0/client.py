801892b5d7f6e7d2b191d67b5cacf4db015baa31
huhuta <smarthorse6@gmail.com>
2018-09-02 18:16:06 +0900

    Add part0 solution

---
+++ projects/proj1_chat/part0/client.py
@@ -0,0 +1,17 @@
+import sys
+import socket
+from time import sleep
+
+
+def main(host, port):
+    s = socket.socket()
+    s.connect((host, int(port)))
+
+    message = input('-> ')
+    s.send(message.encode())
+    s.close()
+
+
+if __name__ == '__main__':
+    host, port = sys.argv[1:]
+    main(host, port)
---
+++ projects/proj1_chat/part0/server.py
@@ -0,0 +1,24 @@
+import sys
+import socket
+
+
+def main(port):
+    serversocket = socket.socket()
+    serversocket.bind(('localhost', int(port)))
+    serversocket.listen(1)
+
+    while True:
+        (c, address) = serversocket.accept()
+        message = c.recv(1024)
+        tmp = message
+        while tmp:
+            tmp = c.recv(1024)
+            print(tmp)
+            message += tmp
+        print(message.decode())
+        c.close()
+
+
+if __name__ == '__main__':
+    port = sys.argv[1]
+    main(port)
